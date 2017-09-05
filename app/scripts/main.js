eabd250b799777db4051ba1d077d9c3bafde1da6
cvidalgatica <cvidalgatica@gmail.com>
2017-09-05 00:01:42 -0300

    Arreglos para fancybox y pelotas en mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,3 +2,9 @@
     $('.parallax').scrolly(
         //{bgParallax: true}
     );
+
+    // función quitar clase si es dispositivo móvil
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    	// mobile!
+    	$('.cursos .card').removeClass('fancybox-pdf');
+	}
